fce26854ad540c85a465e38ee4cb7addc992447b
naoyakouda <nashu5667@gmail.com>
2017-02-12 16:42:16 +0900

    add command

---
+++ command.h
@@ -0,0 +1,26 @@
+#ifndef COMMAND_H_
+#define COMMAND_H_
+
+//lanc command
+#define AutoFocus 0x2841 //Auto-Focus on/off
+#define REC 0x183A //Rec
+
+#define TELE1 0x2800 //zoom Tele: slowest speed
+#define TELE2 0x2802 //zoom Tele: faster than 2800
+#define TELE3 0x2804 //zoom Tele: faster than 2802
+#define TELE4 0x2806 //zoom Tele: faster than 2804
+#define TELE5 0x2808 //zoom Tele: faster than 2806
+#define TELE6 0x280A //zoom Tele: faster than 2808
+#define TELE7 0x280C //zoom Tele: faster than 280A
+#define TELE8 0x280E //zoom Tele: fastest speed
+
+#define WIDE1 0x2810 //zoom Wide: slowest speed
+#define WIDE2 0x2812 //zoom Wide: faster than 2810
+#define WIDE3 0x2814 //zoom Wide: faster than 2812
+#define WIDE4 0x2816 //zoom Wide: faster than 2814
+#define WIDE5 0x2818 //zoom Wide: faster than 2816
+#define WIDE6 0x281A //zoom Wide: faster than 2818
+#define WIDE7 0x281C //zoom Wide: faster than 281A
+#define WIDE8 0x281E //zoom Wide: fastest speed
+
+#endif /* COMMAND_H_ */
